54b69bd8bb72d3ee541f5b63bf7e2ac7c485f325
Bartosz Sokolowski <bartosz.sokolowski@provider.pl>
2022-02-08 11:51:03 +0100

    Tests for scrapper

--- tests/Unit/ComesConnectedScrapperTest.php
+++ tests/Unit/ComesConnectedScrapperTest.php
@@ -2,19 +2,41 @@
 
 use App\Providers\Scrapers\ComesConnectedScrapper;
 
-test('comesconnectedscrapper', function () {
+test('scrapper plugin setResults/getResults test', function () {
     $testArray = [];
     $this->comesConMock =  new ComesConnectedScrapper();
     $this->comesConMock->setResults($testArray);
     expect($this->comesConMock->getResults())->toBeArray();
     expect($this->comesConMock->getResults())->toBe($testArray);
-    expect($this->comesConMock->getJsonResults())->toBeString();
 
+    $testString = 'aaa%bbb';
+    $testExcludeSymbol = '%';
+    expect($this->comesConMock->cleanString($testExcludeSymbol,$testString))->toBeString();
+    expect($this->comesConMock->cleanString($testExcludeSymbol,$testString))->toBe('aaabbb');
+});
+
+test('scrapper plugin getJsonResults test', function () {
+    $testArray = [];
+    $this->comesConMock =  new ComesConnectedScrapper();
+    $this->comesConMock->setResults($testArray);
+    expect($this->comesConMock->getJsonResults())->toBeString();
+});
+
+test('scrapper plugin pushResult test', function () {
+    $testArray = [];
+    $this->comesConMock =  new ComesConnectedScrapper();
     $testArrayElement = ['test'];
     $testArrayFinal = [['test']];
     $this->comesConMock->setResults($testArray);
     $this->comesConMock->pushResult($testArrayElement);
     expect($this->comesConMock->getResults())->toBeArray();
     expect($this->comesConMock->getResults())->toBe($testArrayFinal);
+});
 
+test('scrapper plugin cleanString test', function () {
+    $this->comesConMock =  new ComesConnectedScrapper();
+    $testString = 'aaa%bbb';
+    $testExcludeSymbol = '%';
+    expect($this->comesConMock->cleanString($testExcludeSymbol,$testString))->toBeString();
+    expect($this->comesConMock->cleanString($testExcludeSymbol,$testString))->toBe('aaabbb');
 });
