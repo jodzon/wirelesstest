1c3d233f0a494829bc76bd571edd109c503932ba
Bartosz Sokolowski <bartosz.sokolowski@provider.pl>
2022-02-08 11:58:56 +0100

    Tests for scrapper

--- tests/Feature/ScrapCommandTest.php
+++ tests/Feature/ScrapCommandTest.php
@@ -8,6 +8,27 @@ $notProperURL = 'https://google.com';
 $badURL = 'badurl';
 $properJsonResult = '[{"option_title":"Option 3600 Mins","option_description":"Up to 3600 minutes talk time per year including 480 SMS(5p \/ minute and 4p \/ SMS thereafter)","option_price":"174.00","option_currency":"GBP","option_discount":["Save \u00a318 on the monthly price"]},{"option_title":"Option 2000 Mins","option_description":"Up to 2000 minutes talk time per year including 420 SMS(5p \/ minute and 4p \/ SMS thereafter)","option_price":"108.00","option_currency":"GBP","option_discount":["Save \u00a312 on the monthly price"]},{"option_title":"Option 480 Mins","option_description":"Up to 480 minutes talk time per yearincluding 240 SMS(5p \/ minute and 4p \/ SMS thereafter)","option_price":"66.00","option_currency":"GBP","option_discount":["Save \u00a35 on the monthly price"]},{"option_title":"Option 300 Mins","option_description":"300 minutes talk time per monthincluding 40 SMS(5p \/ minute and 4p \/ SMS thereafter)","option_price":"16.00","option_currency":"GBP","option_discount":["No discount"]},{"option_title":"Option 160 Mins","option_description":"Up to 160 minutes talk time per monthincluding 35 SMS(5p \/ minute and 4p \/ SMS thereafter)","option_price":"10.00","option_currency":"GBP","option_discount":["No discount"]},{"option_title":"Option 40 Mins","option_description":"Up to 40 minutes talk time per monthincluding 20 SMS(5p \/ minute and 4p \/ SMS thereafter)","option_price":"6.00","option_currency":"GBP","option_discount":["No discount"]}]';
 
+test('ScrapCommand class validateUrl test', function () use ($properURL, $badURL) {
+    $this->scrapMock =  new ScrapCommand();
+    $checkUrl = env('VIDEX_SCRAP_URL');
+    expect($this->scrapMock->validateUrl($properURL))->toBeTrue();
+    expect($this->scrapMock->validateUrl($badURL))->toBeNull();
+});
+
+test('ScrapCommand class sortResults test', function () {
+    $this->scrapMock =  new ScrapCommand();
+    $inputArray = [
+        ['option_price' => 1],
+        ['option_price' => 10],
+    ];
+    $expectArray = [
+        ['option_price' => 10],
+        ['option_price' => 1],
+    ];
+    expect($this->scrapMock->sortResults($inputArray))->toBe($expectArray);
+});
+
+
 test('crawler class check', function ($url) {
     $this->client = Mockery::mock(Goutte::class);
     $this->client->shouldReceive('request')->with($url)->andReturn(Symfony\Component\DomCrawler\Crawler::class);
